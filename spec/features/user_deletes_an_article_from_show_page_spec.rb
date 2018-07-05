236e19f1765f5daf6bedab832ef7d1efcb2fca56
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-04 20:54:48 -0600

    Add delete article from a show page feature test

---
+++ spec/features/user_deletes_an_article_from_show_page_spec.rb
@@ -0,0 +1,15 @@
+describe "user deletes an article" do
+  describe "they link from the show page" do
+    it "displays all articles withouted the deleted entry" do
+      article_1 = Article.create(title: 'Deleted Title', body: 'Deleted Body')
+      article_2 = Article.create(title: 'Test Title', body: 'Test Body')
+
+      visit article_path(article_1)
+      click_link 'Delete'
+
+      expect(current_path).to eq('/articles')
+      expect(page).to have_content(article_2.title)
+      expect(page).to_not have_content(article_1.title)
+    end
+  end
+end
