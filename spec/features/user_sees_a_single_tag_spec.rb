0591a729c8c84eb307a7622bddfc9b620956d5f0
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-05 16:27:10 -0600

    Add article tags linking to a tag show page functionality.

---
+++ spec/features/user_sees_a_single_tag_spec.rb
@@ -0,0 +1,11 @@
+describe 'user sees one tag' do
+  it 'clicking from an article show to a tag show displays the tag name' do
+    article = Article.create(title: "New Title", body: "New Body")
+    tag = article.tags.create(name: "Name")
+
+    visit article_path(article)
+    click_link(tag.name)
+
+    expect(page).to have_content(tag.name)
+  end
+end
