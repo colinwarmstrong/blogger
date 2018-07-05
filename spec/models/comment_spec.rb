b786bbb2338e1726a8ce130b4e571322e152637b
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-04 22:09:51 -0600

    Create basic comment migration and model/

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe Comment, type: :model do
-  descibe 'validation' do
+  describe 'validation' do
     it {should belong_to(:article)}
-    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:author_name)}
     it {should validate_presence_of(:body)}
   end
 end
